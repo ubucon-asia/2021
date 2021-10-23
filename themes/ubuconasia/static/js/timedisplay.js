7e1225879daeabaa071564fad71936a7fb46f082
Seongsu Yoon <sople1@snooey.net>
2021-10-23 20:19:05 +0900

    Fix viewing datetime
    Problem in iOS error "Invalid Date", cause by different with ISO-8601 Date Format
    add moment, moment-timezone for fix it.

--- themes/ubuconasia/static/js/timedisplay.js
+++ themes/ubuconasia/static/js/timedisplay.js
@@ -4,6 +4,15 @@ window.addEventListener('load', ()=>{
         let display = element.getAttribute("display");
         let timeSource = element.innerHTML;
         let date = new Date(timeSource);
+        if (date.toString() == "Invalid Date") {
+          let dateStrBlock = timeSource.replace(' ', 'T').split(' ');
+          alert(dateStrBlock.length + ' | ' + dateStrBlock[0] + ' | ' + dateStrBlock[1]);
+          if (dateStrBlock.length == 2) {
+            date = new Date(moment.tz(dateStrBlock[0], dateStrBlock[1]).toDate());
+          } else {
+            date = new Date(moment(dateStrBlock[0]).toDate());
+          }
+        }
         switch(display){
         	case "date":
           	element.innerHTML = date.toLocaleDateString();
