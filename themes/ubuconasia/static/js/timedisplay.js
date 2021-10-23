b5c122249abc89fda2089cfefa9daca6d457b4ff
Seongsu Yoon <sople1@snooey.net>
2021-10-23 22:27:50 +0900

    Fix timedisplay.js
    For parsing different time format. (e.g. 2021-10-13 00:00:00 EST, 2021-10-20 20:00:00 +0900, 2021-10-20 20:00:00 +09:00,)
    
    (cherry picked from commit b961f17f47202f4a6517e4ecd0569d957d5e2d05)

--- themes/ubuconasia/static/js/timedisplay.js
+++ themes/ubuconasia/static/js/timedisplay.js
@@ -4,12 +4,26 @@ window.addEventListener('load', ()=>{
         let display = element.getAttribute("display");
         let timeSource = element.innerHTML;
         let date = new Date(timeSource);
-        if (!(date instanceof Date && !isNaN(date))){
+        if (!(date instanceof Date && !isNaN(date))) {
+          let _changed = null;
+          let is_timezone_text = function (zone) {
+            let m = /^([+-])([0-9]{2}):?([0-9]{2})$/gm.exec(zone.trim());
+            if (m == null) {
+              return true;
+            }
+            
+            _changed = m[1] + m[2] + ':' + m[3];
+            return false;
+          };
           let dateStrBlock = timeSource.replace(' ', 'T').split(' ');
-          if (dateStrBlock.length == 2) {
+          if (dateStrBlock.length == 2 && is_timezone_text(dateStrBlock[1])) {
             date = new Date(moment.tz(dateStrBlock[0], dateStrBlock[1]).toDate());
-          } else {
-            date = new Date(moment(dateStrBlock[0]).toDate());
+          } else if (_changed !== null) {
+            dateStrBlock[1] = _changed;
+          }
+            
+          if (!(date instanceof Date && !isNaN(date))) {
+            date = new Date(moment(dateStrBlock.join('')).toDate());
           }
         }
         switch(display){
